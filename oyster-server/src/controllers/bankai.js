2718ec5206b38b5f2b3559ad2662f3a137143320
Nicolas Vo <nicolasvo95@gmail.com>
2019-08-28 01:16:36 +0200

    remove test script

--- oyster-server/src/controllers/bankai.js
+++
@@ -1,9 +0,0 @@
-const languages = ['en', 'ru', 'es'];
-console.log(languages);
-
-let header = [];
-languages.forEach((language) => {
-  header.push({'text': language, 'value': language});
-});
-
-console.log(header);

f754afb22e51c602ebb4b0e04f1f6cf8aca92421
nico <nicolasvo95@gmail.com>
2020-12-25 17:17:19 +0100

    clean up for refactor

--- oyster-server/test/request.spec.js
+++
@@ -1,9 +0,0 @@
-const nock = require('nock');
-const response = require('./res/response.json');
-
-
-// describe('Get route', function () {
-//   it('should exist', function () {
-//
-//   });
-// });

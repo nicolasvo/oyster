2f2d59abd9d282771cdc0f070724739ffcef30d4
nico <nicolasvo95@gmail.com>
2020-12-27 00:33:23 +0100

    list words

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <div id="content"></div>
+    <p>{{ words }}</p>
   </div>
 </template>
 
@@ -11,80 +11,177 @@ export default {
   props: {
     msg: String,
   },
+  data: () => ({
+    words: "jesus",
+  }),
+  beforeCreate() {
+    handleClientLoad().then(async () => {
+      this.words = await getWords()[0];
+      // this.words = "hmm ziegel";
+    });
+    // addWord("desert", "en");
+    // updateWord("desesrt", 0, 5);
+  },
+  created() {
+  },
   mounted() {
-    handleClientLoad();
   },
 };
 
-function handleClientLoad() {
-  gapi.load("client:auth2", initClient);
-}
+const spreadsheetId = '1CvbZsBsC-vqbqLyM8ov0B5O0SM-D3EB3zo6PJJYUpqs';
+const range = 'A1:Z';
 
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  var CLIENT_ID = "582485760308-aim00v0sor96v66bn2h4rgerqo1f1g74.apps.googleusercontent.com";
-  var API_KEY = "AIzaSyBUANl-jEVX7FlqOIGr3hVYrH3WUwvLFtU";
-  var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4",];
-  var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES,
-  })
-  .then(function () {
-      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    },
-    function (error) {
-      console.log(JSON.stringify(error, null, 2));
-    }
-  );
+function handleClientLoad() {
+  return new Promise((resolve, reject) => {
+    gapi.load("client:auth2", () => {
+      const CLIENT_ID = "582485760308-aim00v0sor96v66bn2h4rgerqo1f1g74.apps.googleusercontent.com";
+      const API_KEY = "AIzaSyBUANl-jEVX7FlqOIGr3hVYrH3WUwvLFtU";
+      const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4",];
+      const SCOPES = "https://www.googleapis.com/auth/drive.file";
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      }).then(() => {
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+          resolve({ isReady: true });
+        },
+        function (error) {
+          console.log(JSON.stringify(error, null, 2));
+        }
+      );
+    });
+  });
 }
 
 function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
-    getWords();
+    console.log("User signed in!");
   } else {
     gapi.auth2.getAuthInstance().signIn();
   }
 }
-const spreadsheetId = '1CvbZsBsC-vqbqLyM8ov0B5O0SM-D3EB3zo6PJJYUpqs';
+
 function getWords() {
-  gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: spreadsheetId,
-    // spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'A1:C',
-  }).then(function(response) {
-    const range = response.result;
-    if (range.values.length > 0) {
-      console.log(range.values.length);
-      const languages = range.values[0];
-      const data = [];
-      let temp = {};
-      const zip = (a, b) => a.map((x, i) => [x, b[i]]);
-      for (const row of range.values.slice(1)) {
-        temp = {};
-        for (const [language, word] of zip(languages, row)) {
-          temp[language] = word;
+  return new Promise((resolve, reject) => {
+    gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: spreadsheetId,
+      range: range,
+    }).then(function(response) {
+      const range = response.result;
+      if (range.values.length > 0) {
+        console.log(range.values.length);
+        const languages = range.values[0];
+        const data = [];
+        let temp = {};
+        const zip = (a, b) => a.map((x, i) => [x, b[i]]);
+        for (const row of range.values.slice(1)) {
+          temp = {};
+          for (const [language, word] of zip(languages, row)) {
+            temp[language] = word;
+          }
+          data.push(temp);
         }
-        data.push(temp);
+        resolve(data);
+      } else {
+        console.log('No data found.');
       }
-      // console.log(data);
-      return data;
-    } else {
-      console.log('No data found.');
-    }
-  }, function(response) {
-    console.log('Error: ' + response.result.error.message);
+    }, function(response) {
+      console.log('Error: ' + response.result.error.message);
+    });
   });
 }
 
-function addWord() {
-  
+async function addWord(word, fromLanguage) {
+  let fromLanguageIndex = -1;
+  const languages = await getLanguages();
+  fromLanguageIndex = languages.indexOf(fromLanguage);
+  console.log(fromLanguageIndex);
+  let values = [];
+  for (let col = 0; col < languages.length; col++) {
+    if (col !== fromLanguageIndex) {
+      values.push(`=GOOGLETRANSLATE("${word}", "${fromLanguage}", "${languages[col]}")`);
+    } else {
+      values.push(word);
+    }
+  }
+  console.log(values);
+  const params = {
+    spreadsheetId: spreadsheetId,
+    range: range,
+    valueInputOption: "USER_ENTERED",  
+  };
+  const valueRangeBody = {
+    range: "A1:Z",
+    majorDimension: "ROWS",
+    values: [values],
+  };
+  await gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody).then(function(response) {
+    console.log("Word added!");
+  });
+}
+
+async function deleteWord(rowIndex) {
+  const params = {
+    spreadsheetId: spreadsheetId,
+  };
+  const requestBody = {
+    requests: [{
+      deleteRange: {
+        range: {
+          startRowIndex: rowIndex,
+          endRowIndex: rowIndex+1,
+        },
+        shiftDimension: "ROWS",
+      }
+    }]
+  };
+  await gapi.client.sheets.spreadsheets.batchUpdate(params, requestBody).then(function(response) {
+    console.log("Word deleted!");
+  });
+}
+
+async function updateWord(word, languageIndex, rowIndex) {
+  const col = columnToLetter(languageIndex + 1);
+  const range = col + (rowIndex + 1);
+  const params = {
+    spreadsheetId: spreadsheetId,
+    range: range,
+    valueInputOption: "USER_ENTERED",
+  };
+  const valueRangeBody = {
+    range: range,
+    majorDimension: "ROWS",
+    values: [[word]],
+  };
+  await gapi.client.sheets.spreadsheets.values.update(params, valueRangeBody).then(function(response) {
+    console.log("Word updated!");
+  });
+}
+
+function getLanguages() {
+  return new Promise((resolve, reject) => {
+    gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: spreadsheetId,
+      range: range,
+    }).then(function(response) {
+      const range = response.result;
+      const languages = range.values[0];
+      resolve(languages);
+    });
+  })
+}
+
+function columnToLetter(column) {
+  let temp, letter = "";
+  while (column > 0) {
+    temp = (column - 1) % 26;
+    letter = String.fromCharCode(temp + 65) + letter;
+    column = (column - temp - 1) / 26;
+  }
+  return letter;
 }
 </script>
 

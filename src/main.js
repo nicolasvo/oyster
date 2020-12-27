08095a0a838f737f533e05a4910f9e35801bbfe8
nico <nicolasvo95@gmail.com>
2020-12-27 12:11:38 +0100

    disable pwa

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import './registerServiceWorker'
+// import './registerServiceWorker'
 
 createApp(App).mount('#app')

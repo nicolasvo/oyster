9d3d17b9a33b2c0a2841282a956c24cbe3aec9de
Nicolas Vo <nicolas.vo@doctolib.com>
2022-04-09 00:14:19 +0200

    add addword button

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -1,6 +1,8 @@
 <script>
     import { languages, words, isSignedIn } from "./store.js";
     import Animation from "./Animation.svelte";
+    import Input from "./Input.svelte";
+    import { each } from "svelte/internal";
 
     const range = "A1:Z";
     const getContent = async (fileId) => {
@@ -90,27 +92,48 @@
         all();
     }
 
-    const addWord = async () => {};
-    const toggleWordDialog = async () => {};
+    function addWord(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        console.log("adding word!!!!");
+        let i = 0;
+        let values = [];
+        formData.forEach((v, k) => {
+            if (v) {
+                if (k != $languages[i]) {
+                }
+                $languages.forEach((l, j) => {
+                    if (k != l) {
+                        values.push(`=GOOGLETRANSLATE("${v}";"${k}";"${l}")`);
+                    }
+                    else {
+                        values.push(v);
+                    }
+                });
+                console.log(values);
+                i++;
+            }
+        });
+        toggleWordForm();
+    }
+    let wordForm = false;
+    const toggleWordForm = async () => {
+        wordForm = !wordForm;
+    };
 </script>
 
 <div class="flex justify-center">
-    <button
-        class="m-5 p-3 text-stone-100 font-semibold rounded-xl shadow-md focus:outline-none bg-greenish hover:bg-greenish-dark"
-    >
-        Add word
-    </button>
-</div>
-<div class="flex justify-center">
     {#if $isSignedIn == true}
         {#if $languages.length}
-            <table class="table-auto overflow-auto overflow-hidden rounded-xl">
+            <table
+                class="m-3 table-auto overflow-auto overflow-hidden rounded-xl"
+            >
                 <thead class="border-g bg-blueish">
                     <tr>
                         {#each $languages as language}
                             <th
                                 scope="col"
-                                class="text-sm font-bold text-stone-100 px-6 py-4 text-center"
+                                class="font-bold text-stone-100 px-6 py-4 text-center"
                             >
                                 {language}
                             </th>
@@ -121,9 +144,7 @@
                     {#each $words as word}
                         <tr class="bg-stone-50 border-b">
                             {#each $languages as language}
-                                <td
-                                    class="text-sm text-blueish p-3 text-center"
-                                >
+                                <td class="text-blueish p-3 text-center">
                                     {word[language]}
                                 </td>
                             {/each}
@@ -134,5 +155,38 @@
         {:else}
             <Animation />
         {/if}
+    {/if}
+</div>
+<div class="flex justify-center">
+    {#if wordForm}
+        <form on:submit={addWord}>
+            <div class="grid content-center">
+                {#each $languages as language}
+                    <div class="p-2">
+                        <Input label={language} name={language} type="text" />
+                    </div>
+                {/each}
+                <div class="grid grid-cols-2">
+                    <button
+                        class="m-3 p-3 text-stone-100 font-semibold rounded-xl shadow-md focus:outline-none bg-blueish hover:bg-blueish-dark"
+                        on:click={toggleWordForm}
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        class="m-3 p-3 text-stone-100 font-semibold rounded-xl shadow-md focus:outline-none bg-blueish hover:bg-blueish-dark"
+                    >
+                        Confirm
+                    </button>
+                </div>
+            </div>
+        </form>
+    {:else}
+        <button
+            class="m-3 p-3 text-stone-100 font-semibold rounded-xl shadow-md focus:outline-none bg-blueish hover:bg-blueish-dark"
+            on:click={toggleWordForm}
+        >
+            Add word
+        </button>
     {/if}
 </div>
